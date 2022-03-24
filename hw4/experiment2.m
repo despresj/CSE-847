9b29aea7cafebc0d1fc1d43d745e6f3814234082
despresj <54477023+despresj@users.noreply.github.com>
2022-03-24 12:10:28 -0400

    finished reg

--- hw4/experiment2.m
+++ hw4/experiment2.m
@@ -1,23 +1,31 @@
 load("data/ad_data.mat")
 
-% Specify the options
+root=cd;
+addpath(genpath([root '/SLEP']));
+
 opts.rFlag = 1;  % range of par within [0, 1].
 opts.tol = 1e-6; % optimization precision
 opts.tFlag = 4;  % termination options.
 opts.maxIter = 5000; % maximum iterations.
 
-addpath(genpath([root '/SLEP']));
-
-pars = 0.001:0.0001:0.1; % regularization values to try
+pars = 0:0.001:1; % regularization values to try
 auc_vec = zeros(1, length(pars));
 for i = 1:numel(pars)
-    [w, c] = LogisticR(X_train, y_train, pars(i), opts);
-     phat = (1 + exp(- (X_test * w + c))); % convert estimates to probs
-     [X,Y,T,AUC] = perfcurve(y_test,phat,1);
-    auc_vec(i) = AUC;
+     [w, c] = LogisticR(X_train, y_train, pars(i), opts);
+     yhat = X_test * w + c;
+     [X,Y,T,AUC] = perfcurve(y_test,yhat, 1);
+       auc_vec(i) = AUC;
 end
+
 [AUC, i] = max(auc_vec);
-[w, c] = LogisticR(X_train, y_train, pars(i), opts); % recaluclate using best value
+[w, c] = LogisticR(X_train, y_train, pars(i), opts) % recaluclate using best value
 AUC
 pars(i)
-sum(w == 0);
+sum(w == 0)
+
+figure;
+plot(pars, auc_vec, '-r');
+title('Logistic Reg Exeriment');
+xlabel('penalty ');
+ylabel('AUC');
+saveas(gcf,'reg_experiment.png');
